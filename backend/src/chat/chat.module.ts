b4973dc4b8e071fd679713c7c94551d27ecb5e87
shizantnu <142576200+shizantnu@users.noreply.github.com>
2025-09-21 17:05:02 +0200

    chat module made

---
+++ backend/src/chat/chat.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ChatController } from './chat.controller';
+import { ChatService } from './chat.service';
+import { ChatGateway } from './chat.gateway';
+import { PrismaService } from '../common/prisma.service';
+
+@Module({
+    controllers: [ChatController],
+    providers: [ChatService, ChatGateway, PrismaService],
+    exports: [ChatService],
+})
+export class ChatModule {}
